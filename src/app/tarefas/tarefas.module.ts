40b5258a195fcf09ee00276deebcadf59a319713
andrewsilva <andrew.keizze@gmail.com>
2020-02-19 23:07:28 -0300

    Ajustes e Cadastro

--- src/app/tarefas/tarefas.module.ts
+++ src/app/tarefas/tarefas.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { TarefaService } from './shared';
 import { ListarTarefaComponent } from './listar';
-
+import { RouterModule } from '@angular/router';
+import { CadastrarTarefaComponent } from './cadastrar';
 
 
 @NgModule({
-  declarations: [ListarTarefaComponent],
+  declarations: [ListarTarefaComponent, CadastrarTarefaComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    FormsModule
   ],
   providers: [
     TarefaService
